0b4e0914873b05882db77cea31be13513ae0ee14
kzeigler <kzeigler@tns.org>
2018-08-01 14:39:39 -0400

    openfn@tns.org OpenFn Web Edit

--- PIMA/BeniBiz_CommCare_SalesForce_Client_Activity_Coaching.js
+++ PIMA/BeniBiz_CommCare_SalesForce_Client_Activity_Coaching.js
@@ -1,65 +1,93 @@
+// Here, we make sure CommCare gives us an array to use in each(merge(...), ...)
+alterState((state) => {
+  const presentClients = state.data.form.Present_Clients;
+  if (!Array.isArray(presentClients)) {
+    state.data.form.Present_Clients = [presentClients];
+  }
+  return state;
+});
 
 each(
-  "$.data",
-  upsert("Client_Activity__c", "Submission_ID__c",
+  "$.data.form.Present_Clients[*]",
+  submit(
     fields(
-      field("Submission_ID__c", function(state) {
-        var submission = dataValue("id")(state)+dataValue("form.client")(state);
-        return submission;
+      field("@", function(state) {
+        return {
+          "xmlns:jrm": "http://dev.commcarehq.org/jr/xforms",
+          "xmlns": "http://openrosa.org/formdesigner/22A693D2-7F34-4B8D-AE12-8A4A57C8868F",
+          "uiVersion": "1",
+          "version": "325",
+          "name": "New Attendance",
+        };
       }),
-      relationship("Client__r", "CommCare_Case_Id__c", dataValue("form.client")),
-      field("RecordTypeId", "0121o00000115AWAAY"),
-      field("Project_Staff__c", dataValue("form.trainer")),
-      field("Date__c", dataValue("form.name_and_date_group.date")),
-      field("Comments__c", function(state) {
-        var topics = dataValue("form.topics_covered")(state);
-        var taskCompleted = dataValue("form.tasks_completed")(state);
-        var taskAssigned = dataValue("form.tasks_assigned")(state);
-        var requests = dataValue("form.participant_requests")(state);
-        var questions = dataValue("form.questions_comments_suggestions")(state);
-        
-        var comment = "Topics Covered: "+topics+"\n"+"\n"+
-        "Tasks Completed: "+taskCompleted+"\n"+"\n"+
-        "Tasks Assigned: "+taskAssigned+"\n"+"\n"+
-        "Participant Requests: "+requests+"\n"+"\n"+
-        "Questions/Comments/Concerns: "+questions;
-        
-        return comment;
-        
-      }),
-      field("Status__c", "Complete"),
-      field("Duration__c", dataValue("form.duration")),
-      field('GPS_Location__Latitude__s', (state) => {
+      field("Date", dataValue("form.Date")),
+      field("Training_Module", dataValue("form.Training_Module")),
+      field("GPS_Coordinates", (state) => {
         // define this once, it's constant...
-        const coords = state.data.form.gps_coordinates;
+        const coords = state.data.form.GPS_Coordinates;
         // write your contional
         if (coords) {
-          return coords.split(" ")[0];
+          return coords;
         }
         // the 'else' is unnecessary, return '' if there are no coords
         return '';
       }),
-      field('GPS_Location__Longitude__s', (state) => {
-        // define this once, it's constant...
-        const coords = state.data.form.gps_coordinates;
-        // write your contional
-        if (coords) {
-          return coords.split(" ")[1];
-        }
-        // the 'else' is unnecessary, return '' if there are no coords
-        return '';
+      field("Photo_URL", dataValue("form.Photo")),
+      field("Signature_URL", dataValue("form.Trainer_Signature")),
+      
+      field("n0:case", function(state) {
+        return {
+          "@": {
+            "case_id": dataValue("form.Present_Clients[*]")(state) + dataValue("id")(state),
+            "date_modified": new Date().toISOString(),
+            "user_id": "e926526fc13b126fffdb6d001f25b269",
+            "xmlns:n0": "http://commcarehq.org/case/transaction/v2"
+          },
+          "n0:create": {
+            "n0:case_name": dataValue("form.Training_Module")(state),
+            "n0:owner_id": "e926526fc13b126fffdb6d001f25b269",
+            "n0:case_type": "zAttendance"
+          },
+          "n0:update": {
+            "n0:Client": dataValue("form.Present_Clients[*]")(state),
+            "n0:Date": dataValue("form.Date"),
+            "n0:Photo_URL": dataValue("form.Photo")(state),
+            "n0:Signature_URL": dataValue("form.Trainer_Signature")(state),
+            "n0:GPS_Coordinates": (state) => {
+              // define this once, it's constant...
+              const coords = state.data.form.GPS_Coordinates;
+              // write your contional
+              if (coords) {
+                return coords;
+              }
+              // the 'else' is unnecessary, return '' if there are no coords
+              return '';
+            },
+          },
+          "n0:index": {
+            "n0:parent" : {
+              "@": {
+                "case_type": "zTrainingGroup"
+              },
+              "#": dataValue("form.case.@case_id")(state)
+            }
+          }
+        };
+      }),
+      field("n1:meta", function(state) {
+        return {
+          "@": {
+            "xmlns:n1": "http://openrosa.org/jr/xforms"
+          },
+          "n1:deviceID": "0",
+          "n1:timeStart": new Date().toISOString(),
+          "n1:timeEnd": new Date().toISOString(),
+          "n1:username": "api",
+          "n1:userID": "e926526fc13b126fffdb6d001f25b269"
+        };
       })
-      )
     )
-  );
-// each(
-//   "$.data",
-//   upsert("Client_Activity__c", "Submission_ID__c",
-//     fields(
-//       field("Submission_ID__c", function(state) {
-//         return dataValue("id")(state)+dataValue("form.client")(state);
-//       },
-//       field("Create_in_CommCare__c", true)
-//     )
-//   )
-// ));
+  )
+);
+
+//Version Control set 8/1/2018
