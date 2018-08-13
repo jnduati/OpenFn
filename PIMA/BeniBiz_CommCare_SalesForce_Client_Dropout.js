41bd06858c558a1e37f3db9dba083276831b774f
kzeigler <kzeigler@tns.org>
2018-08-13 10:58:26 -0400

    openfn@tns.org OpenFn Web Edit

--- PIMA/BeniBiz_CommCare_SalesForce_Client_Dropout.js
+++ PIMA/BeniBiz_CommCare_SalesForce_Client_Dropout.js
@@ -5,7 +5,8 @@ each(
     fields(
       field("CommCare_Case_Id__c", dataValue("form.case.@case_id")),
       field("Status__c", "Inactive"),
-      field("Status_Notes__c", dataValue("form.reasons"))
+      field("Status_Notes__c", dataValue("form.reasons")),
+      field("Status_Change_Date__c", dataValue("form.name_and_date_group.date"))
       )
     )
   );
