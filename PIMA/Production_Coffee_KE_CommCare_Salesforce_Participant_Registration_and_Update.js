1ac5c214526739e5491408cbf57a268f20a41e7a
kzeigler <kzeigler@tns.org>
2018-08-22 07:22:15 -0400

    openfn@tns.org OpenFn Web Edit

--- PIMA/Production_Coffee_KE_CommCare_Salesforce_Participant_Registration_and_Update.js
+++ PIMA/Production_Coffee_KE_CommCare_Salesforce_Participant_Registration_and_Update.js
@@ -1,39 +1,37 @@
+//New Households use this section.
 beta.each(
   "$.data",
-  combine(
-    upsert("Household__c", "Household_ID__c",
+  upsertIf(
+    state.data.form.survey_detail == "New Household",
+    "Household__c", "Household_ID__c",
       fields(
         field("Household_ID__c", dataValue("form.Household_Id")),
         field("Name", dataValue("form.Household_Number")),
         field("Training_Group__c", dataValue("form.Training_Group_Id")),
-        field("Farm_Size__c", function(state){
-          if(dataValue("form.Status")(state) == "Inactive" || dataValue("form.Status")(state) == "Deceased"){
-            return undefined;
-          }
-          else {
-            return dataValue("form.Number_of_Trees")(state);
-          }
-        })
+        field("Farm_Size__c", dataValue("form.Number_of_Trees"))
       )
-    ),
+    )
+  ),
 
-    upsert("Training_Group__c", "CommCare_Case_Id__c",
+  beta.each(
+    "$.data",
+    upsertIf(
+    state.data.form.survey_detail == "New Household",
+    "Training_Group__c", "CommCare_Case_Id__c",
       fields(
         field("Household_Counter__c", dataValue("form.Update_Household_Counter")), 
         field("CommCare_Case_Id__c", dataValue("form.Training_Group_Id"))     
       )
-    ),
+    )
+  ),
   
-    upsert("Participant__c", "CommCare_Case_Id__c",
+  beta.each(
+    "$.data",
+    upsertIf(
+    state.data.form.survey_detail == "New Household",
+    "Participant__c", "CommCare_Case_Id__c",
       fields(
-        field("CommCare_Case_Id__c", function(state){
-          if(dataValue("form.survey_detail")(state) == "New Household" || dataValue("form.survey_detail")(state) == "Existing Household" ){
-            return dataValue("form.subcase_0.case.@case_id")(state);
-          }
-          else {
-            return dataValue("form.case.@case_id")(state);
-          }
-        }),
+        field("CommCare_Case_Id__c", dataValue("form.subcase_0.case.@case_id")),
         field("TNS_Id__c", dataValue("form.Farmer_Id")),
         field("Age__c", dataValue("form.Age")),
         field("Name", dataValue("form.First_Name")),
@@ -41,7 +39,6 @@ beta.each(
         field("Last_Name__c", dataValue("form.Last_Name")),
         field("Gender__c", dataValue("form.Gender")),
         field("Status__c", dataValue("form.Status")),
-        //field("Create_in_CommCare__c", true),
         field("Sent_to_OpenFn_Status__c", "Complete"),
         field("Phone_Number__c", dataValue("form.Phone_Number")),
         field("Other_ID_Number__c", dataValue("form.Cooperative_Membership_Number")),
@@ -49,32 +46,75 @@ beta.each(
         relationship("Household__r", "Household_ID__c", dataValue("form.Household_Id")),
         relationship("Training_Group__r", "CommCare_Case_Id__c", dataValue("form.Training_Group_Id"))
       )
-    ),
-    
-    upsert("Participant__c", "CommCare_Case_Id__c",
+    )
+  ),
+
+//Existing household use this section.
+beta.each(
+  "$.data",
+  upsertIf(
+    state.data.form.survey_detail == "Existing Household",
+    "Household__c", "Household_ID__c",
       fields(
-        field("CommCare_Case_Id__c", function(state){
-          if(dataValue("form.Status")(state) == "Inactive" || dataValue("form.Status")(state) == "Deceased"){
-            return dataValue("form.new_primary_household_member")(state);
-          }
-          else if(dataValue("form.survey_detail")(state) == "New Household" || dataValue("form.survey_detail")(state) == "Existing Household" ){
-            return dataValue("form.subcase_0.case.@case_id")(state);
-          }
-          else {
-            return dataValue("form.case.@case_id")(state);
-          }
-        }),
-        field("Primary_Household_Member__c", function(state){
-          if(dataValue("form.Status")(state) == "Inactive" || dataValue("form.Status")(state) == "Deceased"){
-            return "Yes";
-          } 
-          else {
-            return dataValue("form.Primary_Household_Member")(state);
-          }
-        })
+        field("Household_ID__c", dataValue("form.Household_Id")),
+        field("Farm_Size__c", dataValue("form.Number_of_Trees"))
       )
     )
-  )
-);
+  ),
+
+  beta.each(
+    "$.data",
+    upsertIf(
+    state.data.form.survey_detail == "Existing Household",
+    "Participant__c", "CommCare_Case_Id__c",
+      fields(
+        field("CommCare_Case_Id__c", dataValue("form.subcase_0.case.@case_id")),
+        field("TNS_Id__c", dataValue("form.Farmer_Id")),
+        field("Age__c", dataValue("form.Age")),
+        field("Name", dataValue("form.First_Name")),
+        field("Middle_Name__c", dataValue("form.Middle_Name")),
+        field("Last_Name__c", dataValue("form.Last_Name")),
+        field("Gender__c", dataValue("form.Gender")),
+        field("Status__c", dataValue("form.Status")),
+        field("Sent_to_OpenFn_Status__c", "Complete"),
+        field("Phone_Number__c", dataValue("form.Phone_Number")),
+        field("Other_ID_Number__c", dataValue("form.Cooperative_Membership_Number")),
+        field("Primary_Household_Member__c", dataValue("form.Primary_Household_Member")),
+        relationship("Household__r", "Household_ID__c", dataValue("form.Household_Id")),
+        relationship("Training_Group__r", "CommCare_Case_Id__c", dataValue("form.Training_Group_Id"))
+      )
+    )
+  ),
+
+//Update participants use this section.
+beta.each(
+  "$.data",
+  upsertIf(
+    state.data.form.survey_detail == "Participant Update",
+    "Household__c", "Household_ID__c",
+      fields(
+        field("Household_ID__c", dataValue("form.Household_Id")),
+        field("Farm_Size__c", dataValue("form.Number_of_Trees"))
+      )
+    )
+  ),
+
+  beta.each(
+    "$.data",
+    upsertIf(
+    state.data.form.survey_detail == "Participant Update",
+    "Participant__c", "CommCare_Case_Id__c",
+      fields(
+        field("CommCare_Case_Id__c", dataValue("form.case.@case_id")),
+        field("Age__c", dataValue("form.Age")),
+        field("Name", dataValue("form.First_Name")),
+        field("Middle_Name__c", dataValue("form.Middle_Name")),
+        field("Last_Name__c", dataValue("form.Last_Name")),
+        field("Gender__c", dataValue("form.Gender")),
+        field("Phone_Number__c", dataValue("form.Phone_Number")),
+        field("Other_ID_Number__c", dataValue("form.Cooperative_Membership_Number"))
+      )
+    )
+  );
 
 //Version Control
