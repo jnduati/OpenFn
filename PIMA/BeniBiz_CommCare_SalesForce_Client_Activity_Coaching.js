d1a40d133b31571511bd0e9cc6113a34ebf43494
kzeigler <kzeigler@tns.org>
2018-08-29 10:23:14 -0400

    openfn@tns.org OpenFn Web Edit

--- PIMA/BeniBiz_CommCare_SalesForce_Client_Activity_Coaching.js
+++ PIMA/BeniBiz_CommCare_SalesForce_Client_Activity_Coaching.js
@@ -14,16 +14,8 @@ each(
       field("Tasks_Assigned__c", dataValue("form.tasks_assigned")),
       field("Tasks_Completed__c", dataValue("form.tasks_completed")),
       field("Topic_Covered__c", dataValue("form.topics_covered")),
-      field("Comments__c", function(state) {
-        var requests = dataValue("form.participant_requests")(state);
-        var questions = dataValue("form.questions_comments_suggestions")(state);
-        
-        var comment = "Participant Requests: "+requests+"\n"+"\n"+
-        "Questions/Comments/Concerns: "+questions;
-        
-        return comment;
-        
-      }),
+      field("Client_Request__c", dataValue("form.participant_requests")),
+      field("Comments__c", dataValue("form.questions_comments_suggestions")),
       field("Status__c", "Complete"),
       field("Duration__c", dataValue("form.duration")),
       field('GPS_Location__Latitude__s', (state) => {
